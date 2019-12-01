fa03f1c5219857e6a7f03e4a607238fe8d21ef14
Simon Timms <stimms@gmail.com>
2019-12-01 09:13:22 -0700

    Adding vue lookup

---
+++ ml-prediction/Pages/PetController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ml_prediction.Pages
+{
+    public class PetController : Controller
+    {
+        [Route("/api/pets")]
+        public IActionResult Index()
+        {
+            return new JsonResult(new List<string> { "Cat", "Dog" });
+        }
+    }
+}
